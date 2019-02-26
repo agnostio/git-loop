54dbcb470d2e85fd04723fa6bb6ce5296c8dd968
Adam Meola <adammeola@gmail.com>
2019-02-26 12:23:47 -0500

    Git Loop: 2019/2/26 - 12:23.0

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 const fDate = (d) => {
     // return d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + " - " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-    return `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()} - ${d.getHours()}:${d.getMinutes()}.${d.getSeconds()/60}`;
+
+    return `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()} - ${d.getHours()}:${d.getMinutes()}.${Math.floor(d.getSeconds()/60)}`;
 };
 var shell = require('shelljs')
 const fs = require('fs');
